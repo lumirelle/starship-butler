1cae546a9504c342ddbe142196704d353b8618db
Lumirelle <shabbyacc@outlook.com>
2025-08-20 21:47:42 +0800

    feat(types): improvement

--- packages/types/src/options.ts
+++ packages/types/src/options.ts
@@ -1,10 +1,12 @@
 export interface OptionsBasic {
   /**
    * Show verbose output.
+   * @default false
    */
   verbose?: boolean
   /**
    * Dry run.
+   * @default false
    */
   dryRun?: boolean
 }
