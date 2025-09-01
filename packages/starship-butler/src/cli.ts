9cc2f7548c15a5591cf91536f3091242edc47ead
Lumirelle <shabbyacc@outlook.com>
2025-09-01 17:25:57 +0800

    chore(config-provider): improve help message

--- packages/starship-butler/src/cli.ts
+++ packages/starship-butler/src/cli.ts
@@ -19,15 +19,14 @@ const systemOptions: SystemOptions = {
 }
 
 cli
-  .command('configure-system', 'Let butler configure your system.')
-  .alias('csys')
-  .option('-i, --include [actions]', 'Include certain configure actions')
-  .option('-x, --exclude [actions]', 'Exclude certain configure actions')
-  .option('-f, --force', 'Force configure')
+  .command('csys', 'Let butler configure your system.')
+  .option('-i, --include [actions]', 'Actions you want to include, default: `all`. Notice that, `include` has higher priority than `exclude`.')
+  .option('-x, --exclude [actions]', 'Actions you want to exclude, default: `none`. Notice that, `include` has higher priority than `exclude`.')
+  .option('-f, --force', 'Configure your system forcibly, override the existing configuration with the same name')
   .option('-?, --verbose', 'Show verbose output')
   .option('-d, --dry-run', 'Dry run')
-  .option('-s, --symlink', 'Use symlink instead of copy')
-  .option('-u, --no-fully-update', 'Disable fully update behavior')
+  .option('-s, --symlink', 'Symlink configuration instead of copy and paste')
+  .option('-u, --no-fully-update', 'Disable fully update behavior, default: false')
   .action(async (options: Partial<ConfigProviderOptionsFromCommandLine>) => {
     const mergedOptions = mergeOptions(config, 'config-provider', options)
     await configureSystem(mergedOptions, systemOptions)
