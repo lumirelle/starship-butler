67dc68262d1af4c33d9450f281250958224d8329
Lumirelle <shabbyacc@outlook.com>
2025-09-07 16:38:23 +0800

    feat(config-provider): accept user input for `set` command

--- packages/config-provider/src/set.ts
+++ packages/config-provider/src/set.ts
@@ -1,5 +1,8 @@
 import type { OptionsBasic } from 'starship-butler-types'
+import path from 'node:path'
+import process from 'node:process'
 import consola from 'consola'
+import { globSync } from 'tinyglobby'
 
 /**
  * Options for command `set`.
@@ -33,11 +36,45 @@ export interface SetOptionsFromCommandLine extends SetOptions {}
 
 /**
  * Setting up locally.
+ * @param category
+ * Category of the configuration, optional. Default is `''`.
+ * This option will be transformed to fit glob pattern:
+ * - If category is `*`, will replace it with `**`
+ * - Otherwise, will transform it to `{category}/**`
  * @param source Source path
- * @param target Target path
+ * @param target Target path, Default is current working directory
  * @param options Configuration and command line options
  */
-export async function settingUp(source: string, target: string, options: Partial<SetOptions>): Promise<void> {
+export async function settingUp(category: string | undefined, source: string, target: string | undefined, options: Partial<SetOptions>): Promise<void> {
   // WIP: Setting up locally
-  consola.debug('[starship-butler] Setting up locally with source:', source, ', target:', target, 'and options:', options)
+  consola.debug('[starship-butler] Setting up locally with category:', category, ', source:', source, ', target:', target, 'and options:', options)
+
+  const assetsPath = path.join(import.meta.dirname, '..', 'assets')
+  consola.debug('[starship-butler] Assets path:', assetsPath)
+
+  // Default values
+  if (!category) {
+    category = ''
+  }
+  // If category is `*`, will replace it with `**` to fit glob pattern
+  else if (category === '*') {
+    category = '**/'
+  }
+  // Transform category to fit glob pattern
+  else {
+    category = `${category}/**/`
+  }
+
+  const sourcePattern = `${category}${source}`
+  consola.debug('[starship-butler] Source pattern:', sourcePattern)
+  const matchedFiles = globSync(sourcePattern, {
+    cwd: assetsPath,
+    dot: true,
+  })
+  consola.debug('[starship-butler] Matched files:', matchedFiles)
+
+  if (!target) {
+    target = process.cwd()
+    consola.debug('[starship-butler] Target not specified, using current working directory:', target)
+  }
 }
