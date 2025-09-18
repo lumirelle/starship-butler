b7770b1befcc9df14ce15224ae86514bb40a49ff
Lumirelle <shabbyacc@outlook.com>
2025-09-18 16:50:42 +0800

    chore(config-provider): update cz config

--- packages/config-provider/assets/project/tools/czg/cz.config.js
+++ packages/config-provider/assets/project/tools/czg/cz.config.js
@@ -1,14 +1,46 @@
 // @ts-check
+import fs from 'node:fs'
+import path from 'node:path'
 import { definePrompt } from 'czg'
 
+/* ----------------------------- Generate scopes ---------------------------- */
+
+/**
+ * @param {string} str
+ * @param {number} length
+ */
+function formatName(str, length) {
+  return str + ' '.repeat(length - str.length)
+}
+
+const packagesDir = 'packages'
+const packages = fs.readdirSync(path.resolve(import.meta.dirname, packagesDir))
+const maxLenPackageName = Math.max(...packages.map(pkg => pkg.length)) + 1 // +1 for `:`
+const packageScopes = packages.map(pkg => ({
+  value: pkg,
+  name: `${formatName(`${pkg}:`, maxLenPackageName)} ${packagesDir}/${pkg}`,
+}))
+
+/* --------------------------------- Config --------------------------------- */
+
 export default definePrompt({
-  markBreakingChangeMode: true,
-  allowBreakingChanges: ['feat', 'fix', 'chore'],
   alias: {
     typo: 'docs: fix typos',
     readme: 'docs: update README.md',
+    deps: 'chore: update dependencies',
   },
+
+  scopes: packageScopes,
   scopeOverrides: {
-    // Define your own scope overrides here
+    chore: [
+      ...packageScopes,
+      { value: 'deps', name: `${formatName('deps: ', maxLenPackageName)} A dependencies change` },
+      { value: 'tools', name: `${formatName('tools: ', maxLenPackageName)} A tools and utilities change` },
+    ],
   },
+
+  allowBreakingChanges: ['feat', 'fix', 'chore'],
+  markBreakingChangeMode: true,
+
+  skipQuestions: ['footerPrefix', 'footer', 'confirmCommit'],
 })
