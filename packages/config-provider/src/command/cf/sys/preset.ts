00c933d31bc31721df9945442ff79053ab6eec16
Lumirelle <shabbyacc@outlook.com>
2025-10-23 23:35:05 +0800

    feat(config-provider): add mcp servers for vscode

--- packages/config-provider/src/command/cf/sys/preset.ts
+++ packages/config-provider/src/command/cf/sys/preset.ts
@@ -344,6 +344,7 @@ export const PRESET_ACTIONS: Action[] = [
       const handlerOperations = [
         { source: join('editor', 'vscode', 'default', 'keybindings.json'), target: join(targetFolder, 'keybindings.json') },
         { source: join('editor', 'vscode', 'default', 'settings.json'), target: join(targetFolder, 'settings.json') },
+        { source: join('editor', 'vscode', 'default', 'mcp.json'), target: join(targetFolder, 'mcp.json') },
         { source: join('editor', 'vscode', 'default', 'global.code-snippets'), target: join(targetFolder, 'snippets', 'global.code-snippets') },
         { source: join('editor', 'vscode', 'default', 'comment.code-snippets'), target: join(targetFolder, 'snippets', 'comment.code-snippets') },
       ]
