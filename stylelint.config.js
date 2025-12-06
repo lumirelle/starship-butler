99f0f82b7f59ce8d3865db563597eb0a5eb8e4b5
Lumirelle <shabbyacc@outlook.com>
2025-12-06 22:36:18 +0800

    chore: add playground & update docs config

--- stylelint.config.js
+++ stylelint.config.js
@@ -3,6 +3,7 @@ import lumirelle from '@lumirelle/stylelint-config'
 export default lumirelle({
   scss: true,
   vue: true,
+  ignoreFiles: ['docs/.vitepress/**'],
 }, {
   files: ['**/*.css', '**/*.scss'],
   rules: {
