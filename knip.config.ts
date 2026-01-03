b68f13b9773349666ba6c71d46e34e9a4f14d868
Lumirelle <shabbyacc@outlook.com>
2026-01-03 23:54:43 +0800

    chore: update knip config

---
+++ knip.config.ts
@@ -0,0 +1,40 @@
+import type { KnipConfig } from 'knip'
+
+export default {
+  workspaces: {
+    '.': {
+      // @keep-sorted
+      ignoreDependencies: [
+        '@stylistic/stylelint-config',
+        'bumpp',
+        'lint-staged',
+        'starship-butler',
+        'stylelint-config-html',
+        'stylelint-config-recess-order',
+        'stylelint-scss',
+        'taze',
+      ],
+    },
+    'playground': {
+      // @keep-sorted
+      ignoreDependencies: [
+        'starship-butler',
+      ],
+    },
+    'packages/config-provider': {
+      // @keep-sorted
+      ignoreBinaries: ['nu'],
+    },
+    'docs': {
+      // @keep-sorted
+      ignoreDependencies: [
+        '@iconify-json/svg-spinners',
+        '@unocss/reset',
+        '@vueuse/core',
+        'floating-vue',
+        'pinia',
+        'uno.css',
+      ],
+    },
+  },
+} satisfies KnipConfig
