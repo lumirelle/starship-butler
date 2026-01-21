81932cdca6e827a29ff257d84e17d5e7cc78d653
Lumirelle <shabbyacc@outlook.com>
2026-01-21 17:28:18 +0800

    feat(config-provider): update stylelint config

--- packages/config-provider/assets/project/linter/stylelint/vue/stylelint.config.js
+++ packages/config-provider/assets/project/linter/stylelint/vue/stylelint.config.js
@@ -1,10 +1,3 @@
 import lumirelle from '@lumirelle/stylelint-config'
 
-export default lumirelle({
-  /**
-   * If you prefer using `prettier` to format your styles, set this to `false`.
-   *
-   * If you are using my eslint config with `formmaters: true`, notice that will enable prettier for styles as well.
-   */
-  stylistic: true,
-})
+export default lumirelle()
