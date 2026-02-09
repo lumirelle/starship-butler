27b0987d6b8be3a1638d3e30ca7883ef50de2765
Lumirelle <shabbyacc@outlook.com>
2026-02-09 11:50:41 +0800

    feat(config-provider): better neovim keymaps in vscode

--- packages/config-provider/assets/editor/nvim/lua/config/keymaps_vscode.lua
+++ packages/config-provider/assets/editor/nvim/lua/config/keymaps_vscode.lua
@@ -3,84 +3,6 @@
 local map = vim.keymap.set
 local vscode = require("vscode")
 
--- Use VSCode's cursor move to handle wrapped lines correctly (compatible with fold/unfold)
-map({ "n", "x" }, "j", function()
-  vscode.action("cursorMove", {
-    args = {
-      to = "down",
-      by = vim.v.count == 0 and "wrappedLine" or "line",
-      value = vim.v.count1,
-    }
-  })
-end, { desc = "Down", silent = true })
-map("v", "j", function()
-  vscode.action("cursorMove", {
-    args = {
-      to = "down",
-      by = vim.v.count == 0 and "wrappedLine" or "line",
-      value = vim.v.count1,
-      select = true,
-    }
-  })
-end, { desc = "Down", silent = true })
-map({ "n", "x" }, "<Down>", function()
-  vscode.action("cursorMove", {
-    args = {
-      to = "down",
-      by = vim.v.count == 0 and "wrappedLine" or "line",
-      value = vim.v.count1,
-    }
-  })
-end, { desc = "Down", silent = true })
-map("v", "<Down>", function()
-  vscode.action("cursorMove", {
-    args = {
-      to = "down",
-      by = vim.v.count == 0 and "wrappedLine" or "line",
-      value = vim.v.count1,
-      select = true,
-    }
-  })
-end, { desc = "Down", silent = true })
-map({ "n", "x" }, "k", function()
-  vscode.action("cursorMove", {
-    args = {
-      to = "up",
-      by = vim.v.count == 0 and "wrappedLine" or "line",
-      value = vim.v.count1,
-    }
-  })
-end, { desc = "Up", silent = true })
-map("v", "k", function()
-  vscode.action("cursorMove", {
-    args = {
-      to = "up",
-      by = vim.v.count == 0 and "wrappedLine" or "line",
-      value = vim.v.count1,
-      select = true,
-    }
-  })
-end, { desc = "Up", silent = true })
-map({ "n", "x" }, "<Up>", function()
-  vscode.action("cursorMove", {
-    args = {
-      to = "up",
-      by = vim.v.count == 0 and "wrappedLine" or "line",
-      value = vim.v.count1,
-    }
-  })
-end, { desc = "Up", silent = true })
-map("v", "<Up>", function()
-  vscode.action("cursorMove", {
-    args = {
-      to = "up",
-      by = vim.v.count == 0 and "wrappedLine" or "line",
-      value = vim.v.count1,
-      select = true,
-    }
-  })
-end, { desc = "Up", silent = true })
-
 -- Use VSCode's undo/redo, as Neovim's undo break-points does not work correctly in VSCode
 map("n", "u", function()
   vscode.action("undo")
@@ -91,12 +13,9 @@ map("n", "<C-r>", function()
   vscode.action("cancelSelection")
 end, { desc = "Redo" })
 
--- Use VSCode's toggle comment
-map({ "n", "v" }, "gcc", function()
-  vscode.action("editor.action.commentLine")
-end, { desc = "toggle line comment" })
-
--- Use VSCode's fold/unfold
+-- Use VSCode's fold/unfold, as Neovim's fold commands do not work in VSCode
+-- It's sad that `gj` and `gk` do not work with vscode's folding
+-- `zm` and `zr` are also not supported
 map("n", "za", function()
   vscode.action("editor.toggleFold")
 end, { desc = "Toggle Fold" })
@@ -124,14 +43,14 @@ map("n", "[e", function()
   vscode.action("editor.action.marker.prevInFiles")
 end, { desc = "Go to Previous Problem" })
 
--- plugin: extras/editor/refactoring.lua for vscode
+-- plugin: extras/editor/refactoring.lua implementation in vscode
 map({ "n", "x" }, "<leader>r", function()
   vscode.with_insert(function()
     vscode.action("editor.action.rename")
   end)
 end, { desc = "Rename Symbol" })
 
--- plugin: ../plugins/vscode_multi_cursor.lua for vscode
+-- plugin: ../plugins/vscode_multi_cursor.lua implementation in vscode
 -- I don't know why when I set these keymaps in the plugin keys option,
 -- it will prevent the default keymaps from being disabled.
 -- Create/cancel multiple cursors
