5e2d418b374199d824a02f0e5e375b26327fa742
Lumirelle <shabbyacc@outlook.com>
2026-03-08 17:58:05 +0800

    fix(config-provider): explicit type annotation

--- packages/config-provider/src/constants.ts
+++ packages/config-provider/src/constants.ts
@@ -1,7 +1,7 @@
 import { join } from 'pathe'
 
-export const PKG_ROOT = import.meta.dirname.includes('shared')
+export const PKG_ROOT: string = import.meta.dirname.includes('shared')
   ? join(import.meta.dirname, '..', '..')
   : join(import.meta.dirname, '..')
 
-export const ASSETS_FOLDER = join(PKG_ROOT, 'assets')
+export const ASSETS_FOLDER: string = join(PKG_ROOT, 'assets')
