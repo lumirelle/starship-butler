72bc283b0e323503e86ed231e9cefe4038e91098
Lumirelle <shabbyacc@outlook.com>
2026-05-03 12:12:08 +0800

    build: workaround of building in windows with bun

--- bunup.config.ts
+++ bunup.config.ts
@@ -1,4 +1,5 @@
 import { defineWorkspace } from 'bunup'
+import { isWindows } from 'std-env'
 
 export default defineWorkspace([
   {
@@ -10,6 +11,9 @@ export default defineWorkspace([
     root: 'packages/config-provider',
     config: {
       entry: ['./src/index.ts', './src/constants.ts', './src/command/*/index.ts'],
+      // FIXME(Lumirelle): Bun has a bug with inlined dev dependencies in Windows, see
+      // possible related issue https://github.com/oven-sh/bun/issues/26798
+      packages: isWindows ? 'external' : undefined,
     },
   },
   {
