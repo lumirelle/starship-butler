95b8225d25f89bca0d8a48aefb07a05d9a41adf8
Lumirelle <shabbyacc@outlook.com>
2026-05-03 17:50:57 +0800

    test: use tinyexec instead of bun to perform bash script test

--- packages/config-provider/test/assets/shell/bash/bash.test.ts
+++ packages/config-provider/test/assets/shell/bash/bash.test.ts
@@ -1,20 +1,19 @@
 import { $ } from 'bun'
 import { describe, expect, it } from 'bun:test'
 import { platform } from 'node:os'
+import { x } from 'tinyexec'
 import { isPathExistEnv } from '../../../../src/command/preset/actions/utils'
 
 const isBashAvailable = await isPathExistEnv('bash')
 
 describe('bash profile tests', () => {
-  it.if(isBashAvailable)(
-    'should pass',
-    async () => {
-      $.cwd(import.meta.dirname)
-      const proc = $`bash ./test.sh --silent`
-      const result = (await proc.text()).split('\n')
-      expect(result[0]).toBe(result[1])
-      expect(result[2]).toBe('0')
-    },
-    platform() === 'win32' ? 10_000 : 5000,
-  )
+  it.if(isBashAvailable)('should pass', async () => {
+    $.cwd(import.meta.dirname)
+    // XXX(Lumirelle): Because of Bun's shell cannot find bash.exe in Windows,
+    // we have to use tinyexec to test the bash profile.
+    const proc = await x('bash', ['./test.sh', '--silent'])
+    const result = proc.stdout.split('\n')
+    expect(result[0]).toBe(result[1])
+    expect(result[2]).toBe('0')
+  }, platform() === 'win32' ? 10_000 : 5000)
 })
