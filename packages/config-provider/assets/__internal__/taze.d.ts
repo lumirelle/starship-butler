9bfea7d11aad4fb3fc6c1bf5bc8087d42a64bceb
Lumirelle <shabbyacc@outlook.com>
2026-05-19 14:18:51 +0800

    feat(config-provider)!: remove taze config, because we only use it as a cli tool

--- packages/config-provider/assets/__internal__/taze.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'taze' {
-  /**
-   * Mocked taze `defineConfig` function for placeholder purpose.
-   */
-  export function defineConfig(config: Record<string, any>): Record<string, any>
-}
