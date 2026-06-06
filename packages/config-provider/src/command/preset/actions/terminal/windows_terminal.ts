fb6972a6c9ef4a726739c84127b4afe6a385a9e8
lumirelle <shabbyacc@outlook.com>
2026-06-06 13:32:55 +0800

    feat: use geist font

--- packages/config-provider/src/command/preset/actions/terminal/windows_terminal.ts
+++ packages/config-provider/src/command/preset/actions/terminal/windows_terminal.ts
@@ -19,7 +19,7 @@ export const windowsTerminal: ActionFactory = () => {
     ]),
     posthandler: ({ destination }) => {
       consola.info(
-        `This configuration will use \`'"Comic Mono", "Symbols Nerd Font Mono", "Maple Mono CN"'\` as terminal fonts, \`Nushell\` as default shell. If you don't want to use them, please edit this config \`(${join(destination, 'settings.json')})\` manually or consider not to include this action.`,
+        `This configuration will use \`'"Geist Mono", "Symbols Nerd Font Mono", "Maple Mono CN"'\` as terminal fonts, \`Nushell\` as default shell. If you don't want to use them, please edit this config \`(${join(destination, 'settings.json')})\` manually or consider not to include this action.`,
       )
     },
   }
